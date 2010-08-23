3bebc30cedb2907c3a63df7c3726893360b15fe8
Luis Miranda <lmiranda@atlassian.com>
2010-08-23 18:37:06 +1000

    fixed bug whereby tz wasn't updated unless you reloaded the page
    thie is because i was using jQuery(document).ready() and the page was being loaded in an ajax req

--- src/main/resources/jira-myzone.js
+++ src/main/resources/jira-myzone.js
@@ -36,6 +36,31 @@ if (!this.MyZone) {
             return tz;
         }
     }
+
+    if (typeof MyZone.registerChangeListener !== 'function') {
+        MyZone.registerChangeListener = function(selectId) {
+            var selector = '#' + selectId
+            var canonicalBaseUrl = (function(){
+                var uri = AJS.parseUri(window.location);
+                return uri.protocol + "://" + uri.authority;
+            })();
+
+            jQuery(selector).change(function() {
+                jQuery(selector + ' option:selected').each(function() {
+                    var selectedTZ = jQuery(this).val()
+
+                    jQuery.ajax({
+                        url: canonicalBaseUrl + contextPath + '/rest/myzone/1.0/prefs',
+                        type: 'PUT',
+                        data: JSON.stringify({ tzId: selectedTZ }),
+                        contentType: "application/json; charset=utf-8",
+                        success: function() { alert('Selected timezone: ' + selectedTZ) },
+                        error: function() { alert('Error setting timezone') }
+                    })
+                })
+            })
+        }
+    }
 }());
 
 jQuery(document).ready(function() {
@@ -51,7 +76,6 @@ jQuery(document).ready(function() {
     jQuery(".date").add(".attachment-date").each(function() {
         var timeString = jQuery.trim(this.innerHTML)
         var draw = function(contents, trigger, showPopup) {
-            // TODO (LGM) remove hardcoded URL
             jQuery.ajax({
                 url: canonicalBaseUrl + contextPath + '/rest/myzone/1.0/convert',
                 type: 'POST',
@@ -68,22 +92,6 @@ jQuery(document).ready(function() {
 
         var options = { onHover: true, showDelay: 400, hideDelay: 400, closeOthers: false, width: 200 }
         AJS.InlineDialog(jQuery(this), "jira-myzone-" + i++, draw, options)
-    })
-
-    // update the prefs whenever the timezone is changed
-    jQuery('#myzone-select-timezone').change(function() {
-        jQuery('#myzone-select-timezone option:selected').each(function() {
-            var selectedTZ = jQuery(this).val()
-
-            jQuery.ajax({
-                url: canonicalBaseUrl + contextPath + '/rest/myzone/1.0/prefs',
-                type: 'PUT',
-                data: JSON.stringify({ tzId: selectedTZ }),
-                contentType: "application/json; charset=utf-8",
-                success: function() { alert('Selected timezone: ' + selectedTZ) },
-                error: function() { alert('Error setting timezone') }
-            })
-        })
     })
 
 })
