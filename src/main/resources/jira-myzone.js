0ee01728960d56fa3e4077d4fdc87101cfade17e
Luis Miranda <lmiranda@atlassian.com>
2010-08-25 09:29:33 +1000

    removed MyZone.getTZ method

--- src/main/resources/jira-myzone.js
+++ src/main/resources/jira-myzone.js
@@ -5,38 +5,6 @@ if (!this.MyZone) {
 
 // create the methods in a closure to avoid creating global variables.
 (function () {
-    if (typeof MyZone.getTZ !== 'function') {
-        // Based on a script by Josh Fraser
-        // http://www.onlineaspect.com/2007/06/08/auto-detect-a-time-zone-with-javascript/
-        MyZone.getTZ = function() {
-            var rightNow = new Date();
-            var jan1 = new Date(rightNow.getFullYear(), 0, 1, 0, 0, 0, 0);  // jan 1st
-            var june1 = new Date(rightNow.getFullYear(), 6, 1, 0, 0, 0, 0); // june 1st
-            var temp = jan1.toGMTString();
-            var jan2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
-            temp = june1.toGMTString();
-            var june2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
-            var std_time_offset = (jan1 - jan2) / (1000 * 60 * 60);
-            var daylight_time_offset = (june1 - june2) / (1000 * 60 * 60);
-            var dst;
-            if (std_time_offset == daylight_time_offset) {
-                dst = false; // daylight savings time is NOT observed
-            } else {
-                // positive is southern, negative is northern hemisphere
-                var hemisphere = std_time_offset - daylight_time_offset;
-                if (hemisphere >= 0)
-                    std_time_offset = daylight_time_offset;
-                dst = true; // daylight savings time is observed
-            }
-
-            var tz = {};
-            tz["offset"] = std_time_offset;
-            tz["dst"] = dst;
-
-            return tz;
-        }
-    }
-
     if (typeof MyZone.registerChangeListener !== 'function') {
         MyZone.registerChangeListener = function(selectId) {
             var selector = '#' + selectId
@@ -70,7 +38,6 @@ jQuery(document).ready(function() {
     })();
 
     var now = new Date().getTime()
-    var tz = MyZone.getTZ()
     var i = 0
 
     jQuery(".date").add(".attachment-date").each(function() {
