81a046e3833ef238a2bfb20ca9980a90c3daf8c5
Luis Miranda <lmiranda@atlassian.com>
2010-10-06 13:31:38 +1100

    refactored jira-myzone.js

--- src/main/resources/jira-myzone.js
+++ src/main/resources/jira-myzone.js
@@ -5,6 +5,14 @@ if (!this.MyZone) {
 
 // create the methods in a closure to avoid creating global variables.
 (function () {
+    /**
+     * Counter for generating unique popup id's.
+     */
+    var popupSequence = 0
+    
+    /**
+     * Registers a change listener for the <select> box in the profile panel.
+     */
     if (typeof MyZone.registerChangeListener !== 'function') {
         MyZone.registerChangeListener = function(selectId) {
             var selector = '#' + selectId
@@ -24,42 +32,47 @@ if (!this.MyZone) {
             })
         }
     }
-}());
+
+    /**
+     * Converts a date node to the user's preferred time zone.
+     */
+    if (typeof MyZone.convertDate !== 'function') {
+        MyZone.convertDate = function($date) {
+            var serverTimeString = AJS.$.trim($date.text())
+
+            AJS.$.ajax({
+                url: contextPath + '/rest/myzone/1.0/convert',
+                type: 'POST',
+                data: JSON.stringify({ time: serverTimeString }),
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function(reply) {
+                    if (reply.time != '') {
+                        // write the converted date and underline it
+                        $date.html(AJS.format('<span style="border-bottom: 1px dotted #bebebe;">{0}</span>', reply.time))
+
+                        // add a pop-up with the original date
+                        var draw = function(contents, trigger, showPopup) {
+                            contents.empty()
+                            contents.append(AJS.format('{0}: {1}', reply.label, serverTimeString))
+                            showPopup()
+                        }
+
+                        var options = { onHover: true, showDelay: 400, hideDelay: 400, closeOthers: false, width: 200 }
+                        AJS.InlineDialog($date, "jira-myzone-" + popupSequence++, draw, options)
+                    }
+                }
+            });
+        }
+    }
+
+}())
 
 AJS.$(document).ready(function() {
-    var now = new Date().getTime()
-    var i = 0
 
+    // convert all dates found on the page
     AJS.$(".date, .attachment-date").each(function() {
-        var dateNode = this
-        var jQueryNode = jQuery(this)
-        var jiraTime = dateNode.innerHTML
-        var timeString = jQuery.trim(jiraTime)
-
-        AJS.$.ajax({
-            url: contextPath + '/rest/myzone/1.0/convert',
-            type: 'POST',
-            data: JSON.stringify({ time: timeString }),
-            dataType: 'json',
-            contentType: "application/json; charset=utf-8",
-            success: function(converted) {
-                if (converted.time != '') {
-                    // write the converted date and underline it
-                    dateNode.innerHTML = converted.time
-                    jQueryNode.wrapInner('<span style="border-bottom: dotted 1px #bebebe;">')
-
-                    // add a pop-up with the original date
-                    var draw = function(contents, trigger, showPopup) {
-                        contents.empty()
-                        contents.append(AJS.format('{0}: {1}', converted.label, timeString))
-                        showPopup()
-                    }
-
-                    var options = { onHover: true, showDelay: 400, hideDelay: 400, closeOthers: false, width: 200 }
-                    AJS.InlineDialog(jQueryNode, "jira-myzone-" + i++, draw, options)
-                }
-            }
-        });
+        MyZone.convertDate(AJS.$(this))
     })
 
 })
